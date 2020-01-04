f62f96ffdd6eafe76edcad5d1bf50586e6ee3901
antonino.calderone <antonino.calderone@workday.com>
2020-01-04 21:32:36 +0000

    Reindent config.h

--- include/config.h
+++ include/config.h
@@ -19,24 +19,26 @@
 #define __HTTP_CONFIG_H__
 
 #ifdef WIN32
-#define HTTP_SERVER_WROOT "~/httpsrv"
+#define HTTP_SERVER_WROOT             "~/httpsrv"
 #else
-#define HTTP_SERVER_WROOT "~/.httpsrv"
+#define HTTP_SERVER_WROOT             "~/.httpsrv"
 #endif
-#define HTTP_SERVER_PORT 8080
-#define HTTP_SERVER_NAME "httpsrv"
-#define HTTP_SERVER_MAJ_V 1
-#define HTTP_SERVER_MIN_V 0
-#define HTTP_SERVER_TX_BUF_SIZE 0x100000
-#define HTTP_SERVER_BACKLOG SOMAXCONN
-#define HTTP_CONNECTION_TIMEOUT_MS 10
-#define HTTP_SERVER_VER "HTTP/1.1"
+#define HTTP_SERVER_PORT              8080
+#define HTTP_SERVER_NAME              "httpsrv"
+#define HTTP_SERVER_MAJ_V             1
+#define HTTP_SERVER_MIN_V             0
+#define HTTP_SERVER_TX_BUF_SIZE       0x100000
+#define HTTP_SERVER_BACKLOG           SOMAXCONN
+#define HTTP_CONNECTION_TIMEOUT_MS    10
+#define HTTP_SERVER_VER               "HTTP/1.1"
 
 #define HTTP_SERVER_POST_STORE       "/store"
 #define HTTP_SERVER_GET_FILES        "/files"
 #define HTTP_SERVER_GET_MRUFILES     "/mrufiles"
 #define HTTP_SERVER_GET_MRUFILES_ZIP "/mrufiles/zip"
 
+
+/* -------------------------------------------------------------------------- */
 
 #endif // __HTTP_CONFIG_H__
 
