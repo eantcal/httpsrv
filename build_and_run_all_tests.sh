e4d2d476d793a857e25da86e288634af86ab4183
Antonino Calderone <antonino.calderone@gmail.com>
2021-01-17 11:31:55 +0000

    Improve c++17 and other dependencies building script support

--- build_and_run_all_tests.sh
+++ build_and_run_all_tests.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 echo This script has been designed to execute on Ubuntu-like Linux distribution
-echo "Install any needed package (root password required)"
-sudo apt install build-essential g++ cmake libboost-all-dev jsonlint curl unzip
+echo New C++ compiler supporting C++17 is a requirement too.
+echo If you want to install GNU compiler version 10 you can use the following commands, valid for Ubuntu 18.04 or higher:
+echo
+echo sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+echo sudo apt install gcc-10 gcc-10-base gcc-10-doc g++-10 libstdc++-10-dev libstdc++-10-doc 
+echo
+echo Make sure symlinks point to the new toolchain before proceeding with cmake commands executing the following commands:
+ls -la /usr/bin/ | grep -oP "[\S]*(gcc|g\+\+)(-[a-z]+)*[\s]" | xargs bash -c 'for link in ${@:1}; do echo sudo ln -s -f "/usr/bin/${link}-${0}" "/usr/bin/${link}"; done' 10
+echo 
+echo "Install any other dependencies (root password required)"
+sudo apt install build-essential g++ cmake libboost-all-dev jsonlint curl unzip uuid-dev
 
 set -x
 
